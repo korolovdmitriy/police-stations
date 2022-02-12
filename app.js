21c02c15835272d108a762b1edc36de16e8f482f
Dmitriy Korolov <adrorecords@gmail.com>
2022-02-12 13:39:14 +0200

    add config cassandra

--- app.js
+++ app.js
@@ -4,25 +4,9 @@ const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const cassandra = require("cassandra-driver");
 const policeStationsRouter = require("./routes/policeStationsRouter");
+const options = require("./swagger-config.json");
 
 const PORT = process.env.PORT || 3000;
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "My Guard",
-      version: "1.0.0",
-      description: "Police stations API",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
 
 const specs = swaggerJsDoc(options);
 
