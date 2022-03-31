39789e4647d0c9c9c0ff4a79ad9058a4837c85b8
Dmitriy Korolov <adrorecords@gmail.com>
2022-03-31 16:45:22 +0300

    add crime server config

--- services/crimeService.js
+++ services/crimeService.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 const { checkCrimes } = require("./checkCrimes");
+const crimeServer = require("../configs/crime-server-config.json");
 
 module.exports = {
   async getAllCrimesByPoliceStationId(id) {
-    const url = `https://tranquil-taiga-07587.herokuapp.com/crimes?policeStationId=${id}`;
+    const url = `${crimeServer.url}/crimes?policeStationId=${id}`;
     return await axios.get(url);
   },
 
