8ef0321dc28e281e238278d1d5adb28f1611c163
Dmitriy Korolov <adrorecords@gmail.com>
2022-04-06 17:42:20 +0300

    update

--- middlewares/checkPoliceRole-middleware.js
+++ middlewares/checkPoliceRole-middleware.js
@@ -2,7 +2,7 @@ const ApiError = require("../exceptions/api-error");
 
 module.exports = function (req, res, next) {
   try {
-    if (req.user !== "police") {
+    if (req.user !== "POLICE") {
       return next(ApiError.AccessDenied());
     }
     next();
