760c4f0e813ad9ffac4078c6f5d374db0961bebe
Dmitriy Korolov <adrorecords@gmail.com>
2022-04-12 11:43:48 +0300

    add catch

--- middlewares/checkToken-middleware.js
+++ middlewares/checkToken-middleware.js
@@ -1,23 +1,27 @@
 const ApiError = require("../exceptions/api-error");
 const securityService = require("../services/securityService");
 
-module.exports = function (req, res, next) {
-  const authorizationHeader = req.headers.authorization;
-  const accessToken = authorizationHeader.split(" ")[1];
-  securityService.checkToken(accessToken).then((result) => {
+// module.exports = function (req, res, next) {
+//   const authorizationHeader = req.headers.authorization;
+//   const accessToken = authorizationHeader.split(" ")[1];
+//   securityService.checkToken(accessToken).then((result) => {
+//     if (result.data.message === "false") {
+//       return next(ApiError.UnauthorizedError());
+//     }
+//     next();
+//   });
+// };
+
+module.exports = async function (req, res, next) {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const accessToken = authorizationHeader.split(" ")[1];
+    const result = await securityService.checkToken(accessToken);
     if (result.data.message === "false") {
       return next(ApiError.UnauthorizedError());
     }
     next();
-  });
+  } catch (error) {
+    return next(ApiError.BadRequest(error));
+  }
 };
-
-// module.exports = async function (req, res, next) {
-//   const authorizationHeader = req.headers.authorization;
-//   const accessToken = authorizationHeader.split(" ")[1];
-//   const result = await securityService.checkToken(accessToken);
-//   if (result.data.message === "false") {
-//     return next(ApiError.UnauthorizedError());
-//   }
-//   next();
-// };
--- services/securityService.js
+++ services/securityService.js
@@ -4,6 +4,6 @@ const securityServer = require("../configs/security-server-config.json");
 module.exports = {
   async checkToken(token) {
     const url = `${securityServer.url}/check?accessToken=${token}`;
-    return await axios.get(url);    
+    return await axios.get(url);
   },
 };
