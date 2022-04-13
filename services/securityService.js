7d4f16a7ce97f486feac6075701b958e14eebaeb
Dmitriy Korolov <adrorecords@gmail.com>
2022-04-13 15:34:54 +0300

    update check token

--- services/securityService.js
+++ services/securityService.js
@@ -2,8 +2,12 @@ const axios = require("axios").default;
 const securityServer = require("../configs/security-server-config.json");
 
 module.exports = {
-  async checkToken(token) {
-    const url = `${securityServer.url}/check?accessToken=${token}`;
-    return await axios.get(url);
+  async checkToken(accessToken) {
+    const url = `${securityServer.url}/auth/check`;
+    return await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
   },
 };
